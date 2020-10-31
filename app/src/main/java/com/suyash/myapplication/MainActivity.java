af1ee9bdc421927857d99ea7bf17c6ac2c1ad88e
Suyash Nehete <suyashnehete78@gmail.com>
2020-10-31 21:06:44 +0530

    with component

--- app/src/main/java/com/suyash/myapplication/MainActivity.java
+++ app/src/main/java/com/suyash/myapplication/MainActivity.java
@@ -9,15 +9,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    static Button btn;
     static Handler handler = new Handler(Looper.getMainLooper());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
     }
 
     public void onClick(View v){
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
+                btn.setText("Its Working");
                 Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
             }
         });
